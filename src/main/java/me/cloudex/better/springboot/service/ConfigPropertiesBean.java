0e6e6472d204a8e674ac70b0aa403f2af630151a
changedi <jysoftware@gmail.com>
2017-06-01 17:40:13 +0800

    update

--- src/main/java/me/cloudex/better/springboot/service/ConfigPropertiesBean.java
+++ src/main/java/me/cloudex/better/springboot/service/ConfigPropertiesBean.java
@@ -3,12 +3,14 @@ package me.cloudex.better.springboot.service;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by zunyuan.jy on 15/05/2017.
  */
 @Component
+@Profile("prod")
 public class ConfigPropertiesBean {
 
     @Value("${product.name}")
